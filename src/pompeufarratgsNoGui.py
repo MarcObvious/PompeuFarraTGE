56fa1362322aefcb9a385441d8f92b58934be82a
Marc <cat166@gmail.com>
2015-06-03 17:18:00 +0200

    let's get this shit started

--- src/pompeufarratgsNoGui.py
+++ src/pompeufarratgsNoGui.py
@@ -1,11 +1,14 @@
 import nltk;
-from nltk.corpus import treebank
+#from nltk.corpus import treebank
 import sys;
 
-for line in sys.stdin:
-    print("has esrit:", line)
-    tokens = nltk.word_tokenize(line)
-    print(tokens)
-    tagged = nltk.pos_tag(tokens)
-    print (tagged[0:len(tagged)])
-    
+def main():
+    print("Write something:")  
+    for line in sys.stdin:
+        print("has esrit:", line)
+        tokens = nltk.word_tokenize(line)
+        print(tokens)
+        tagged = nltk.pos_tag(tokens)
+        print (tagged[0:len(tagged)])
+if __name__ == "__main__":
+    main()
