580d864c98395c618d2ab319e34123c21d970767
Marc <cat166@gmail.com>
2015-06-03 17:48:14 +0200

    run draw resposta

--- src/pompeufarratgsNoGui.py
+++ src/pompeufarratgsNoGui.py
@@ -2,11 +2,25 @@ import nltk;
 #from nltk.corpus import treebank
 import sys;
 
+def resposta(tokens, estat):
+    resposta = 'merda'
+    estatSeguent = 0
+    print("ara intentarem trobar resposta")
+    return resposta, estatSeguent
+
 def sortida(tokens):
     if ('surtJA' in tokens): 
         print ("Good bye")
         exit()
 
+def draw():
+    print()
+
+def run(self):
+#     loop
+    print()
+    
+#A main em de definir tots els estats on podem anar i carregar els valors inicials
 def main():
     print("Write something:")  
     for line in sys.stdin:
@@ -17,7 +31,6 @@ def main():
         print (tagged[0:len(tagged)])
         sortida(tokens)
         print("What are you talking about? Try again")
-        
 
 if __name__ == "__main__":
     main()
